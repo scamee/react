132990b01f7e22d4c034c2fdcdb9d28d25661ed1
scamee <zero.monnhann@gmail.com>
2022-08-21 19:08:09 +0900

    タスクの削除

--- src/App.js
+++ src/App.js
@@ -9,21 +9,35 @@ function App() {
   const handleAddTodo = () => {
     //タスクを追加する。
     const name = todoNameRef.current.value;
+    if (name === "") return;
     setTodos((prevTodos) => {
       return [...prevTodos, { id: uuidv4(), name: name, completed: false }];
     });
   };
 
-  const [todos, setTodos] = useState([
-    { id: uuidv4(), name: "Todo1", completed: false },
-  ]);
+  //checkbox判定
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  };
+
+
+  //完了タスクの削除
+  const handleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  };
+
+  const [todos, setTodos] = useState([]);
   return (
     <>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input type="text" ref={todoNameRef}></input>
       <button onClick={handleAddTodo}>タスクの追加</button>
-      <button>完了したタスクの削除</button>
-      <div>残りのタスク:0</div>
+      <button onClick={handleClear}>完了したタスクの削除</button>
+      <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
     </>
   );
 }
